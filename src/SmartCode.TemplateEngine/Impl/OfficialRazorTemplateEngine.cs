651c2e598a7383fb823476aa967f7ade78229231
Ahoo Wang <ahoowang@qq.com>
2018-10-18 01:01:35 +0800

    add OfficialRazorTemplateEngine

---
+++ src/SmartCode.TemplateEngine/Impl/OfficialRazorTemplateEngine.cs
@@ -0,0 +1,99 @@
+﻿//*******************************
+// Thx https://github.com/aspnet/Entropy/tree/master/samples/Mvc.RenderViewToString
+//*******************************
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Internal;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.ObjectPool;
+using Microsoft.Extensions.WebEncoders;
+using SmartCode.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+using System.Threading.Tasks;
+
+namespace SmartCode.TemplateEngine.Impl
+{
+    public class OfficialRazorTemplateEngine : ITemplateEngine
+    {
+        public bool Initialized { get; private set; }
+        public string Name { get; private set; } = "OfficialRazor";
+        private string _root = AppPath.Relative("OfficialRazorTemplates");
+        private IServiceScopeFactory _scopeFactory;
+        public void Initialize(IDictionary<string, string> paramters)
+        {
+            Initialized = true;
+            if (paramters != null)
+            {
+                if (paramters.TryGetValue("Name", out string name))
+                {
+                    Name = name;
+                }
+                if (paramters.TryGetValue("Root", out string root))
+                {
+                    _root = root;
+                }
+            }
+            InitializeServices();
+        }
+
+        public Task<string> Render(BuildContext context)
+        {
+            using (var serviceScope = _scopeFactory.CreateScope())
+            {
+                var helper = serviceScope.ServiceProvider.GetRequiredService<OfficialRazorViewToStringRenderer>();
+                return helper.RenderViewToStringAsync(context.Build.Template, context);
+            }
+        }
+
+        private void InitializeServices()
+        {
+            var services = ConfigureDefaultServices();
+            var serviceProvider = services.BuildServiceProvider();
+            _scopeFactory = serviceProvider.GetRequiredService<IServiceScopeFactory>();
+        }
+        private IServiceCollection ConfigureDefaultServices()
+        {
+            var services = new ServiceCollection();
+            IFileProvider fileProvider = new PhysicalFileProvider(_root);
+            services.AddSingleton<IHostingEnvironment>(new HostingEnvironment
+            {
+                ApplicationName = Assembly.GetEntryAssembly().GetName().Name,
+                WebRootFileProvider = fileProvider,
+            });
+            //services.Configure<WebEncoderOptions>(options =>
+            //      {
+            //          var txtEncoderSettings = new TextEncoderSettings(UnicodeRanges.All);
+            //          //txtEncoderSettings.Clear();
+            //          txtEncoderSettings.AllowCharacter('<');
+            //          txtEncoderSettings.AllowCharacter('>');
+            //          txtEncoderSettings.AllowCharacter('&');
+            //          txtEncoderSettings.AllowCharacter('\''); // can be used to escape attributes
+            //          txtEncoderSettings.AllowCharacter('\"'); // can be used to escape attributes
+            //          txtEncoderSettings.AllowCharacter('+');
+            //          options.TextEncoderSettings = txtEncoderSettings;
+            //      }
+            //    );
+
+            services.Configure<RazorViewEngineOptions>(options =>
+            {
+                options.FileProviders.Clear();
+                options.FileProviders.Add(fileProvider);
+            });
+            var diagnosticSource = new DiagnosticListener("Microsoft.AspNetCore");
+            services.AddSingleton<ObjectPoolProvider, DefaultObjectPoolProvider>();
+            services.AddSingleton<DiagnosticSource>(diagnosticSource);
+            services.AddLogging();
+            services.AddMvc();
+            services.AddTransient<OfficialRazorViewToStringRenderer>();
+            return services;
+        }
+    }
+}
---
+++ src/SmartCode.TemplateEngine/OfficialRazorExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.AspNetCore.Mvc.Rendering
+{
+    public static class OfficialRazorExtensions
+    {
+        public static IHtmlContent NewLine(this IHtmlHelper htmlHelper)
+        {
+            return htmlHelper.Raw(Environment.NewLine);
+        }
+
+        public static IHtmlContent PadLeft(this IHtmlHelper htmlHelper, int totalWidth)
+        {
+            return htmlHelper.Raw(String.Empty.PadLeft(totalWidth));
+        }
+        public static IHtmlContent PadRight(this IHtmlHelper htmlHelper, int totalWidth)
+        {
+            return htmlHelper.Raw(String.Empty.PadRight(totalWidth));
+        }
+    }
+}
