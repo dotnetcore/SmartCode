18cad8423ca55d56c6eb66c0165b92e2cdfcce6a
Ahoo Wang <ahoowang@qq.com>
2018-10-20 14:50:57 +0800

    add 构建整个解决方案，直接运行API项目进行Swagger调试！！！

---
+++ src/SmartCode/Utilities/NamingUtil.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartCode
+{
+    public class NamingUtil
+    {
+        public static String CamelCase(string phrase)
+        {
+            string firstChar = phrase.Substring(0, 1).ToLower();
+            return firstChar + phrase.Substring(1);
+        }
+    }
+}
