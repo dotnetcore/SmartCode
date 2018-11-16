2f74621ea127e16017a27831e2899c47d55590a2
Ahoo <ahoowang@qq.com>
2018-11-16 11:04:18 +0800

    1. support for Sqlserver paging syntax By [Offset Fetch]

---
+++ src/SmartCode.ETL/Entity/Monitor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartCode.ETL.Entity
+{
+    public class Monitor
+    {
+        public string BuidKey { get; set; }
+
+        public string MachineName { get; }
+
+        public string ProcessName { get; }
+
+        public long NonpagedSystemMemorySize64 { get; }
+        public long PagedMemorySize64 { get; }
+        public long PagedSystemMemorySize64 { get; }
+        public long WorkingSet64 { get; }
+        public long VirtualMemorySize64 { get; }
+        public TimeSpan UserProcessorTime { get; }
+        public TimeSpan TotalProcessorTime { get; }
+        
+        public TimeSpan PrivilegedProcessorTime { get; }
+        public long PrivateMemorySize64 { get; }
+        public bool PriorityBoostEnabled { get; set; }
+        public long PeakWorkingSet64 { get; }
+        public long PeakVirtualMemorySize64 { get; }
+        public long PeakPagedMemorySize64 { get; }
+    }
+}
