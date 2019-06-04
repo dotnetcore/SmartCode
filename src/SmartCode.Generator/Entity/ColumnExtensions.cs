e38f4b9c7d0f6727e627fac5c121a9e91245ea4e
beginor <beginor@qq.com>
2019-06-05 07:07:10 +0800

    add basic typescript template

---
+++ src/SmartCode.Generator/Entity/ColumnExtensions.cs
@@ -0,0 +1,16 @@
+namespace SmartCode.Generator.Entity {
+
+    public static class ColumnExtensions {
+
+        /// <summary>
+        /// 获取列的摘要说明，如果 Description 不为空， 则返回 Description；
+        /// 否则返回 Name + DbType
+        /// </summary>
+        public static string GetSummary(this Column column) {
+            if (!string.IsNullOrEmpty(column.Description)) {
+                return column.Description;
+            }
+            return column.Name + ", " + column.DbType;
+        }
+    }
+}
