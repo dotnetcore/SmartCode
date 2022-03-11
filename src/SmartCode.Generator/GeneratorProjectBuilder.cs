704734fe3c83239169081984e4099d9f6a53aedf
yuanwj <yuanweijie@auto11.com>
2022-03-11 16:54:44 +0800

    添加多少线程构建，添加WaitPre

--- src/SmartCode.Generator/GeneratorProjectBuilder.cs
+++ src/SmartCode.Generator/GeneratorProjectBuilder.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using SmartCode.Configuration;
@@ -9,6 +11,7 @@ namespace SmartCode.Generator
         private readonly Project _project;
         private readonly IPluginManager _pluginManager;
         private readonly ILogger<GeneratorProjectBuilder> _logger;
+        private CountdownEvent countdown = new CountdownEvent(1);
 
         public GeneratorProjectBuilder(
             Project project
@@ -23,25 +26,49 @@ namespace SmartCode.Generator
 
         public async Task Build()
         {
-            BuildContext buildContext = null;
             var dataSource = _pluginManager.Resolve<IDataSource>(_project.DataSource.Name);
             await dataSource.InitData();
+
+            this.countdown.Reset();
             foreach (var buildKV in _project.BuildTasks)
             {
-                _logger.LogInformation($"-------- BuildTask:{buildKV.Key} Start! ---------");
-                var output = buildKV.Value.Output;
-                buildContext = new BuildContext
+                if (!buildKV.Value.WaitPre)
                 {
-                    PluginManager = _pluginManager,
-                    Project = _project,
-                    DataSource = dataSource,
-                    BuildKey = buildKV.Key,
-                    Build = buildKV.Value,
-                    Output = output?.Copy()
-                };
-                await _pluginManager.Resolve<IBuildTask>(buildKV.Value.Type).Build(buildContext);
-                _logger.LogInformation($"-------- BuildTask:{buildKV.Key} End! ---------");
+                    this.countdown.AddCount();
+                    Thread t = new Thread((obj) =>
+                    {
+                        var p = ((KeyValuePair<string, Build> buildKV, IDataSource dataSource))obj;
+                        BuildTask(buildKV, dataSource);
+                        this.countdown.Signal();
+                    });
+
+                    t.Start((buildKV, dataSource));
+                    continue;
+                }
+
+                this.countdown.Signal();
+                this.countdown.Wait();
+                this.countdown.Reset();
+
+                BuildTask(buildKV, dataSource);
             }
+        }
+
+        private void BuildTask(KeyValuePair<string, Build> buildKV, IDataSource dataSource)
+        {
+            _logger.LogInformation($"-------- BuildTask:{buildKV.Key} Start! ---------");
+            var output = buildKV.Value.Output;
+            BuildContext buildContext = new BuildContext
+            {
+                PluginManager = _pluginManager,
+                Project = _project,
+                DataSource = dataSource,
+                BuildKey = buildKV.Key,
+                Build = buildKV.Value,
+                Output = output?.Copy()
+            };
+            _pluginManager.Resolve<IBuildTask>(buildKV.Value.Type).Build(buildContext).Wait();
+            _logger.LogInformation($"-------- BuildTask:{buildKV.Key} End! ---------");
         }
     }
 }
